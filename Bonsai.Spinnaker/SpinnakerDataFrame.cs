ab259d167e9f8e856d3b11819451efa6b0804d29
glopes <glopes@localhost>
2017-06-17 18:11:28 +0100

    Added support for image metadata.

---
+++ Bonsai.Spinnaker/SpinnakerDataFrame.cs
@@ -0,0 +1,23 @@
+﻿using OpenCV.Net;
+using SpinnakerNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Spinnaker
+{
+    public class SpinnakerDataFrame
+    {
+        public SpinnakerDataFrame(IplImage image, ManagedChunkData chunkData)
+        {
+            Image = image;
+            ChunkData = chunkData;
+        }
+
+        public IplImage Image { get; private set; }
+
+        public ManagedChunkData ChunkData { get; private set; }
+    }
+}
