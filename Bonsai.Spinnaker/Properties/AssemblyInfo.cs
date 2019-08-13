cddb1939a56705ef13dc5471878a93401a274024
glopes <glopesdev@users.noreply.github.com>
2019-08-13 19:41:06 +0100

    Allow derived classes to initialize and configure the camera node map.

--- Bonsai.Spinnaker/Properties/AssemblyInfo.cs
+++ Bonsai.Spinnaker/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gonçalo Lopes")]
 [assembly: AssemblyProduct("Bonsai.Spinnaker")]
-[assembly: AssemblyCopyright("Copyright © Gonçalo Lopes 2017")]
+[assembly: AssemblyCopyright("Copyright © Gonçalo Lopes 2017-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
-[assembly: AssemblyInformationalVersion("0.4.1")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyInformationalVersion("0.5.0")]
