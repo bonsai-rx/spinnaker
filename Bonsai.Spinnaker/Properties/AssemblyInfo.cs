235ff0c68b9b5196d07bc22276ab9902d9e55de3
glopes <glopesdev@users.noreply.github.com>
2019-11-01 00:54:05 +0000

    Updated core dependencies and added default package icon.

--- Bonsai.Spinnaker/Properties/AssemblyInfo.cs
+++ Bonsai.Spinnaker/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using Bonsai;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -35,3 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.5.0.0")]
 [assembly: AssemblyFileVersion("0.5.0.0")]
 [assembly: AssemblyInformationalVersion("0.5.0")]
+[assembly: WorkflowNamespaceIcon("Bonsai:ElementIcon.Video")]
