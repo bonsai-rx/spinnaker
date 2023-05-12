bf810ba01ea7fe53282bbe81b8619dc237f1dc71
Cristian Goina <goinac@hhmi.org>
2023-05-11 21:24:58 -0400

    set all ColorProcessingAlgorithm enums from 1.4

--- Bonsai.Spinnaker/ColorProcessingAlgorithm.cs
+++ Bonsai.Spinnaker/ColorProcessingAlgorithm.cs
@@ -2,7 +2,33 @@
 {
     public enum ColorProcessingAlgorithm
     {
+        /** Default method. */
         Default,
-        NoColorProcessing
+        /** No color processing. */
+        NoColorProcessing,
+        /**
+         * Fastest but lowest quality. Equivalent to
+         * FLYCAPTURE_NEAREST_NEIGHBOR_FAST in FlyCapture.
+         */
+        NearestNeighbor,
+        /**
+         * Nearest Neighbor with averaged green pixels. Higher quality but slower
+         * compared to nearest neighbor without averaging.
+         */
+        NearestNeighborAvg,
+        /** Weighted average of surrounding 4 pixels in a 2x2 neighborhood. */
+        Bilinear,
+        /** Weights surrounding pixels based on localized edge orientation. */
+        EdgeSensing,
+        /** Well-balanced speed and quality. */
+        HqLinear,
+        /** Multi-threaded with similar results to edge sensing. */
+        Ipp,
+        /** Best quality but much faster than rigorous. */
+        DirectionalFilter,
+        /** Slowest but produces good results. */
+        Rigorous,
+        /** Weighted pixel average from different directions. */
+        WeightedDirectionalFilter
     }
 }
